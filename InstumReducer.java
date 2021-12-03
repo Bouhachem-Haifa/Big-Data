fde983c23f72d0f97bbf2b655d3dfd110f126495
Bouhachem-Haifa <92925195+Bouhachem-Haifa@users.noreply.github.com>
2021-12-03 17:23:56 +0100

    Update InstumReducer.java

--- InstumReducer.java
+++ InstumReducer.java
@@ -1,21 +1,21 @@
 package tn.isima.tp1;
 
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
 public class IntSumReducer
-        extends Reducer<Text,IntWritable,Text,IntWritable> {
+        extends Reducer< Text, DoubleWritable,Text,DoubleWritable> {
 
-    private IntWritable result = new IntWritable();
+    private IntWritable result = new DoubleWritable();
 
-    public void reduce(Text key, Iterable<IntWritable> values,
+    public void reduce(Text key, Iterable<DoubleWritable> values,
                        Context context
     ) throws IOException, InterruptedException {
         int sum = 0;
-        for (IntWritable val : values) {
+        for (DoubleWritable val : values) {
             System.out.println("value: "+val.get());
             sum += val.get();
         }
